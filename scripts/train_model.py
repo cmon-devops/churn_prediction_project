3916a854a7acf5b20143daac15c277729e54ba4a
cmon-devops <rishmarun18@gmail.com>
2024-11-14 12:21:17 +0530

    Update train_model.py

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -1,1 +1,43 @@
+import numpy as np
+import pandas as pd
+
+class LogisticRegression:
+    def __init__(self, learning_rate=0.01, epochs=1000):
+        self.learning_rate = learning_rate
+        self.epochs = epochs
+        self.weights = None
+        self.bias = None
+
+    def _sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def fit(self, X, y):
+        num_samples, num_features = X.shape
+        self.weights = np.zeros(num_features)
+        self.bias = 0
+
+        for _ in range(self.epochs):
+            linear_model = np.dot(X, self.weights) + self.bias
+            y_predicted = self._sigmoid(linear_model)
+
+            dw = (1 / num_samples) * np.dot(X.T, (y_predicted - y))
+            db = (1 / num_samples) * np.sum(y_predicted - y)
+
+            self.weights -= self.learning_rate * dw
+            self.bias -= self.learning_rate * db
+
+    def predict(self, X):
+        linear_model = np.dot(X, self.weights) + self.bias
+        y_predicted = self._sigmoid(linear_model)
+        return [1 if i > 0.5 else 0 for i in y_predicted]
+
+if __name__ == "__main__":
+    data = pd.read_csv('data/processed_data.csv')
+    X = data.drop('is_churn', axis=1).values
+    y = data['is_churn'].values
+
+    model = LogisticRegression()
+    model.fit(X, y)
+    np.save('scripts/model_weights.npy', model.weights)
+    np.save('scripts/model_bias.npy', model.bias)
 
